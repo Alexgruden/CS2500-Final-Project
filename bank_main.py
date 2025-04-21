2823f6bc587bcda74ba5de7f9d9266b2a379ae17
alexgruden <agruden@uvm.edu>
2025-04-21 18:24:53 -0400

    fixed original data

--- bank_main.py
+++ bank_main.py
@@ -6,22 +6,24 @@ con = sqlite3.connect('bank_sim.db')
 
 cur = con.cursor()
 
-#remove table during subsequent runs
-cur.execute("DROP TABLE IF EXISTS users;")
-cur.execute("DROP TABLE IF EXISTS followers;")
-cur.execute("DROP TABLE IF EXISTS posts;")
+def load_data(input):
+    if input == True:
+        #remove table during subsequent runs
+        cur.execute("DROP TABLE IF EXISTS customers;")
+        cur.execute("DROP TABLE IF EXISTS accounts;")
+        cur.execute("DROP TABLE IF EXISTS loans;")
 
-#load each csv into a data frame
-users = pd.read_csv("users(in).csv")
-followers = pd.read_csv("followers(in).csv")
-posts = pd.read_csv("posts(in).csv")
+        #load each csv into a data frame
+        customers = pd.read_csv("customers.csv")
+        accounts = pd.read_csv("accounts.csv")
+        loans = pd.read_csv("loans.csv")
 
-#insert each csv into a table in the database 
-users.to_sql('users', con, if_exists='replace', index = False)
-followers.to_sql('followers', con, if_exists='replace', index = False)
-posts.to_sql('posts', con, if_exists='replace', index = False)
+        #insert each csv into a table in the database 
+        customers.to_sql('customers', con, if_exists='replace', index = False)
+        accounts.to_sql('accounts', con, if_exists='replace', index = False)
+        loans.to_sql('loans', con, if_exists='replace', index = False)
 
-con.commit()
-con.close()
+        con.commit()
+        con.close()
 
 
